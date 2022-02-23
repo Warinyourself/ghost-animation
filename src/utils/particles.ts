2c4bd7936c9215c19651a5bf5e324f9080498b12
Warinyourself <warinyourself@gmail.com>
2022-02-24 02:05:41 +0300

    Create particles module

---
+++ src/utils/particles.ts
@@ -0,0 +1,58 @@
+import * as THREE from "three";
+import { redLightColor } from "./colors";
+
+const pointLights: THREE.PointLight[] = [];
+const position = 3;
+const timeDuration = 500;
+const directions = ["x", "y", "z"] as const;
+const randomize = (min: number, max: number) =>
+  Math.random() * (max - min) + min;
+
+function buildConst() {
+  const timeRange = timeDuration * randomize(0.8, 1.6);
+  const positionRange = position * randomize(0.9, 1.4);
+  return { timeRange, positionRange };
+}
+
+let randomTime: any[] = [];
+
+export function generateParticles(scene: THREE.Scene) {
+  const sphere = new THREE.SphereGeometry(0.03, 16, 8);
+  for (let i = 0; i <= 8; i++) {
+    const light = new THREE.PointLight(redLightColor, 0.8, 10);
+    const meshLight = new THREE.Mesh(
+      sphere,
+      new THREE.MeshBasicMaterial({ color: redLightColor })
+    );
+    light.add(meshLight);
+
+    scene.add(light);
+    pointLights.push(light);
+  }
+
+  randomTime = pointLights.map(_ => {
+    return {
+      x: buildConst(),
+      y: buildConst(),
+      z: buildConst()
+    };
+  });
+}
+
+export function animatePointLight(
+  pointLight: any,
+  time: number,
+  index: number
+) {
+  directions.forEach(direction => {
+    const { timeRange, positionRange } = randomTime[index][direction];
+
+    pointLight.position[direction] = Math.sin(time / timeRange) * positionRange;
+  });
+}
+
+export function particlesAnimate(time: number) {
+  pointLights.forEach((point, index) => {
+    animatePointLight(point, time, index);
+  });
+}
