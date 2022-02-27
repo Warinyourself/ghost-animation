33db5ba09e08fa45374f3713cdc3fb599ca389a9
Warinyourself <warinyourself@gmail.com>
2022-02-28 01:13:57 +0300

    Add some hearts to the scene

--- src/utils/canvas.ts
+++ src/utils/canvas.ts
@@ -9,13 +9,16 @@ import {
 } from "./animations";
 import { generateParticles, particlesAnimate } from "./particles";
 import { buildLight } from "./light";
-import { buildGUI } from "./gui";
+import { buildPositionFolder } from "./gui";
 
 let mouseX = 0;
 let mouseY = 0;
 const groupGhost = new THREE.Group();
-let leftEyeMesh: Mesh, rightEyeMesh: Mesh, nimbusMesh: any;
+let leftEyeMesh: Mesh, rightEyeMesh: Mesh;
+let heartMesh: Mesh;
 const ghostNames = ["Body", "eyeLeft", "eyeRight"];
+
+groupGhost.userData.clickable = true;
 
 function onMouseMove(event: MouseEvent) {
   event.preventDefault();
@@ -29,9 +32,35 @@ document.addEventListener("mousemove", onMouseMove, false);
 let camera: PerspectiveCamera, scene: Scene, renderer: WebGLRenderer;
 const isDebugMode = true;
 const showHelpers = false;
-const showParticles = false;
-
+const showParticles = true;
 const heartName = "Heart";
+const raycaster = new THREE.Raycaster();
+const clickMouse = new THREE.Vector2();
+
+function intersect(pos: THREE.Vector2) {
+  raycaster.setFromCamera(pos, camera);
+  return raycaster.intersectObjects(scene.children);
+}
+
+function onClick(event: MouseEvent) {
+  clickMouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  clickMouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  const found = intersect(clickMouse);
+  console.log({ found });
+
+  if (found.length > 0) {
+    const lastObject = found.pop()?.object;
+    const isClickable = lastObject && lastObject.userData.clickable;
+    const isGhost = lastObject && lastObject.name === "Body_1";
+
+    if (isClickable || isGhost) {
+      console.log(`found draggable ${found}`);
+    }
+  }
+}
+
+document.addEventListener("click", onClick, false);
 
 function animation(time: number) {
   time += 0.01;
@@ -65,12 +94,10 @@ export function init(canvas?: HTMLCanvasElement) {
 
   camera.position.y = 1;
   camera.position.x = 4.12;
-  camera.position.z = 0;
-
-  camera.rotation.x = 0;
   camera.rotation.y = 1.57;
-  camera.rotation.z = 0;
-  buildGUI(camera);
+  camera.lookAt(-10, 0, 0);
+
+  buildPositionFolder(camera, "camera");
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0x999999);
@@ -97,15 +124,8 @@ export function init(canvas?: HTMLCanvasElement) {
     gltf.scene.traverse(object => {
       const mesh = object as Mesh;
       if (mesh.name === heartName) {
-        setTimeout(() => {
-          scene.add(mesh);
-        });
-        const heartSize = 0.15;
-        setHeartAnimation(mesh);
-        return mesh.scale.set(heartSize, heartSize, heartSize);
-      }
-
-      if (ghostNames.includes(mesh.name)) {
+        heartMesh = mesh;
+      } else if (ghostNames.includes(mesh.name)) {
         if (mesh.name === "eyeLeft") {
           leftEyeMesh = mesh;
         } else if (mesh.name === "eyeRight") {
@@ -113,8 +133,6 @@ export function init(canvas?: HTMLCanvasElement) {
         }
 
         ghostMeshes.push(mesh);
-      } else if (mesh.name === "Nimbus") {
-        nimbusMesh = mesh;
       } else {
         console.log({ name: mesh.name, type: mesh.type });
       }
@@ -128,10 +146,61 @@ export function init(canvas?: HTMLCanvasElement) {
 
     window.addEventListener("resize", onWindowResize);
 
-    setBreatheAnimation(groupGhost);
     groupGhost.add(...ghostMeshes);
+    setBreatheAnimation(groupGhost);
     scene.add(groupGhost);
-    console.log({ scene });
+
+    const coords = [
+      {
+        x: 1.4,
+        y: 0.25,
+        z: 0.5,
+        material: new THREE.MeshStandardMaterial({
+          color: new THREE.Color("#3FC1C9"),
+          roughness: 0.35,
+          metalness: 0.15
+        })
+      },
+      {
+        x: -0.51,
+        size: 0.4,
+        y: 2.86,
+        z: -3.43,
+        color: new THREE.Color("#FC5185")
+      },
+      {
+        x: 2.63,
+        y: 0.05,
+        z: -1.75,
+        size: 0.3,
+        color: new THREE.Color("#A267AC")
+      },
+      {
+        x: 0.5,
+        y: 1.5,
+        size: 0.3,
+        z: 5,
+        color: new THREE.Color("#3282B8")
+      }
+    ];
+
+    for (let i = 0; i < 4; i++) {
+      const coord = coords[i];
+      const defaultMaterial = new THREE.MeshStandardMaterial({
+        color: coord.color,
+        roughness: 0.35,
+        metalness: 0.55
+      });
+      const { x, y, z, material, size } = {
+        size: 0.15,
+        material: defaultMaterial,
+        ...coord
+      };
+      const heart = new THREE.Mesh(heartMesh.geometry, material);
+      heart.scale.set(size, size, size);
+      heart.position.set(x, y, z);
+      scene.add(heart);
+    }
   });
 
   if (!canvas) {
